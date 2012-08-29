242e4c5f845d7d9c83cbcfe4a8c1ba98cc9140ae
Simon Carpentier <simon.carpentier@spacebar.ca>
2012-08-29 11:20:59 -0400

    First commit

---
+++ UnitTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NaturalSort
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void SimpleList()
+        {
+            var list = new List<string>
+                {
+                    "0.12",
+                    "12",
+                    "0.012",
+                    "10",
+                    "Potato",
+                    "1.2",
+                    "1",
+                    "1.2E-4",
+                    "0.0012",                
+                };
+
+            list.Sort(new NaturalSortComparer<string>());
+
+            Assert.AreEqual(list[0], "1.2E-4");
+            Assert.AreEqual(list[1], "0.0012");
+            Assert.AreEqual(list[2], "0.012");
+            Assert.AreEqual(list[3], "0.12");
+            Assert.AreEqual(list[4], "1");
+            Assert.AreEqual(list[5], "1.2");
+            Assert.AreEqual(list[6], "10");
+            Assert.AreEqual(list[7], "12");
+            Assert.AreEqual(list[8], "Potato");
+        }
+
+        [TestMethod]
+        public void ComplexList()
+        {
+            var o1 = new { Id = 1, Value = "0.01" };
+            var o2 = new { Id = 2, Value = "12" };
+            var o3 = new { Id = 3, Value = "1" };
+            var o4 = new { Id = 4, Value = "10" };
+
+            var list = new[] { o1, o2, o3, o4 }.ToList();
+
+            list = list.OrderByNatural("Value").ToList();
+
+            Assert.AreEqual(list[0].Value, "0.01");
+            Assert.AreEqual(list[1].Value, "1");
+            Assert.AreEqual(list[2].Value, "10");
+            Assert.AreEqual(list[3].Value, "12");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+C# Natural Sort
+==============
+Sorting for Humans : Natural Sort Order.
+Supports decimals and exponents.
+
+Usage
+--------------
+For simple lists:
+	list.Sort(new NaturalSortComparer<string>());
+
+For list of complex objects:
+	list = list.OrderByNatural("Value").ToList();
+
+Credits
+--------------
+Justin Jones http://www.codeproject.com/Articles/22517/Natural-Sort-Comparer
+James McCormack http://zootfroot.blogspot.ca/2009/09/natural-sort-compare-with-linq-orderby.html
+Jeff Atwood http://www.codinghorror.com/blog/2007/12/sorting-for-humans-natural-sort-order.html
